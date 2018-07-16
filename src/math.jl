b7507ba5f3d95aad8c08f5318b79662f956b7f5e
Peter M. Haverty <phaverty@gene.com>
2018-07-16 08:12:21 -0700

    Bugfix for countmap, new test

--- src/math.jl
+++ src/math.jl
@@ -2,15 +2,15 @@
 
 function countmap(x::RLEVector{T1,T2}) where {T1,T2}
   tally = Dict{T1,T2}()
-  for i in 1:nrun(x)
-    tally[x.runvalues[i]] = get(tally,x.runvalues[i],0) + x.runvalues[i]
+  for (v,w) in zip(values(x), widths(x))
+    tally[v] = get(tally,v,0) + w
   end
-  return(tally)
+  tally
 end
 
 function mode(x::RLEVector)
   tally = countmap(x)
   which_max = indmax(collect(values(tally)))
   max_val = collect(keys(tally))[ which_max ]
-  return(max_val)
+  max_val
 end
--- test/test_math.jl
+++ test/test_math.jl
@@ -5,9 +5,11 @@ using RLEVectors
 
 @testset begin
 
-# mode and implicitly countmap
-x = RLEVector([4,5,6,5],cumsum([10,5,3,6]))
-@test mode(x) == 5
+    x = RLEVector(["a","b","c","b"],[2,4,6,10])
+    m = countmap(x)
+    @test collect(m) == ["c" => 2, "b" => 6, "a" => 2]
+    x = RLEVector([4,5,6,5],cumsum([10,5,3,6]))
+    @test mode(x) == 5
 
 end # testset
 
