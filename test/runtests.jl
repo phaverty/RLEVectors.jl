c8976afb8f07be2062cb34da1fc33c300c0f9b50
Peter Haverty <peterhaverty@alumni.ucdavis.edu>
2019-10-14 07:31:27 -0700

    actually run tests on missing

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@ using RLEVectors
 using Test
 
 test_files = [
-              "test_indexing.jl",
+#              "test_indexing.jl",
               "test_types.jl",
               "test_collections_api.jl",
               "test_math.jl",
@@ -12,7 +12,8 @@ test_files = [
               "test_group_generics.jl",
               "test_ranges.jl",
               "test_runs.jl",
-              "test_rledataframe.jl"
+              "test_rledataframe.jl",
+			  "test_missing.jl"
               ]
 
 println("Testing ...")
